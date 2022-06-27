fbc7c34dfcb3a57d9377346d01ed32a2eb9c2950
kyleplo <31634240+kyleplo@users.noreply.github.com>
2022-06-27 10:37:45 -0400

    Add library

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # elucidate
 Simple, light syntax highlighter for the web.
+
+## Usage
+Import the library as an ES6 module
+```js
+import elucidate from "./elucidate"
+```
+
+Get syntax highlighting HTML
+```js
+elucidate(`
+  alert("hello world");
+`, "javascript");
+// <span class=elucidate-default...
+```
+The language argument is optional, if ommitted a language-agnostic mode will be used.
+
+The HTML includes 6 classes for various syntax features
+- `elucidate-default` - basic content
+- `elucidate-symbol` - symbols
+- `elucidate-keyword` - keywords
+- `elucidate-string` - strings
+- `elucidate-number` - numbers
+- `elucidate-comment` - comment
+
+CSS for these classes is not included
+
+## Language Support
+Elucidate supports the following programming languages
+- `actionscript` - ActionScript
+- `c` - C
+- `c#`/`cs`/`csharp` - C#
+- `c++`/`cpp` - C++
+- `coffeescript` - CoffeeScript
+- `css` - CSS
+- `csv` - CSV
+- `golang`/`go` - Golang
+- `html`/`htm` - HTML
+- `java` - Java
+- `javascript`/`js`/`jsx` - JavaScript
+- `json` - JSON
+- `lua` - Lua
+- `markdown`/`md` - Markdown
+- `php` - PHP
+- `python`/`py` - Python
+- `ruby` - Ruby
+- `scss`/`sass` - SCSS
+- `tsv` - TSV
+- `typescript`/`ts` - TypeScript
+- `xml` - XML
+- `yaml`/`yml` - YAML
+
+
+## Adding a Language
+Languages are added in the `languages.js` file and can be submitted via a pull request
+
+Each language has the following properties
+- `extend` - Another language string on the list for this language to inherit properties from - if omitted, inherits from the `default` language - note that inherited arrays are not appended - the newer array replaces the old one
+- `strings` - Array of strings that delimit strings
+- `escape` - Array of strings that when placed before a string delimiter, cause the delimiter to be ignored
+- `symbols` - Array of strings used as symbols - case insensitive
+- `keywords` - Array of strings used as keywords - case insensitive - will only be detected when surrounded by not-letter characters
+- `multiLineComment` - Array of two strings that mark the beginning and end of a segment to be ignored by the parser, or an empty array
+- `singleLineComment` - String placed at the beginning of a line causing the parser to ignore the remainder of the line
---
+++ elucidate.js
@@ -0,0 +1,96 @@
+import languages from "./languages";
+
+function makeTags(cls){
+  return "</span><span class=elucidate-" + cls + ">"
+}
+
+function resolveLanguageProperty(language, property){
+  return property in languages[language] ? languages[language][property] : (languages[language].extend ? resolveLanguageProperty(languages[language].extend, property) : resolveLanguageProperty("default", property));
+}
+
+export default (code, languageName = "agnostic") => {
+  const language = {
+    strings: resolveLanguageProperty(languageName, "strings"),
+    escape: resolveLanguageProperty(languageName, "escape"),
+    symbols: resolveLanguageProperty(languageName, "symbols"),
+    keywords: resolveLanguageProperty(languageName, "keywords"),
+    singleLineComment: resolveLanguageProperty(languageName, "singleLineComment"),
+    multiLineComment: resolveLanguageProperty(languageName, "multiLineComment")
+  };
+  var output = "";
+  var i = 0;
+
+  var multiLineComment = false;
+  var stringBlock = null;
+  var singleLineComment = false;
+  var mayBeDefault = true;
+
+  while(i < code.length){
+    const next = code.slice(i, i + 20);
+    const comment = multiLineComment || singleLineComment;
+    const commentOrString = comment || stringBlock;
+
+    if(!comment && language.strings && language.strings.find(e => next.startsWith(e)) && !stringBlock){
+      stringBlock = language.strings.find(e => next.startsWith(e));
+      mayBeDefault = false;
+      output += makeTags("string") + stringBlock;
+      i += stringBlock.length;
+    }else if(!comment && stringBlock === language.strings.find(e => next.startsWith(e))){
+      output += stringBlock;
+      mayBeDefault = true;
+      i += stringBlock.length;
+      stringBlock = null;
+    }else if(!comment && stringBlock && language.escape.find(e => next.startsWith(e) && next.slice(e.length).startsWith(stringBlock))){
+      const escapeBlock = language.escape.find(e => next.startsWith(e)) + stringBlock;
+      output += escapeBlock;
+      i += escapeBlock.length;
+    }else if(!commentOrString && language.multiLineComment && next.startsWith(language.multiLineComment[0])){
+      multiLineComment = true;
+      mayBeDefault = false;
+      output += makeTags("comment") + language.multiLineComment[0];
+      i += language.multiLineComment[0].length;
+    }else if(multiLineComment && next.startsWith(language.multiLineComment[1])){
+      multiLineComment = false;
+      output += language.multiLineComment[1];
+      mayBeDefault = true;
+      i += language.multiLineComment[1].length;
+    }else if(!commentOrString && language.singleLineComment && next.startsWith(language.singleLineComment)){
+      singleLineComment = true;
+      mayBeDefault = false;
+      output += makeTags("comment") + language.singleLineComment;
+      i += language.singleLineComment.length;
+    }else if(singleLineComment && (code[i] === "\n" || code[i] === "\r")){
+      singleLineComment = false;
+      output += "<br>";
+      mayBeDefault = true;
+      i += 1;
+    }else if(!commentOrString && language.symbols.find(e => next.toLowerCase().startsWith(e.toLowerCase()))){
+      const symbolLength = language.symbols.find(e => next.toLowerCase().startsWith(e.toLowerCase())).length;
+      output += makeTags("symbol") + next.slice(0, symbolLength);
+      mayBeDefault = true;
+      i += symbolLength;
+    }else if(!commentOrString && language.keywords.find(e => next.toLowerCase().startsWith(e.toLowerCase()) && (!code[i + e.length] || code[i + e.length].toLowerCase() === code[i + e.length].toUpperCase())) && (i === 0 || code[i - 1].toLowerCase() === code[i - 1].toUpperCase())){
+      const keywordLength = language.keywords.find(e => next.toLowerCase().startsWith(e.toLowerCase())).length;
+      output += makeTags("keyword") + next.slice(0, keywordLength);
+      mayBeDefault = true;
+      i += keywordLength;
+    }else if(!commentOrString && (!isNaN(parseFloat(next)) || !isNaN(parseInt(next)))){
+      const numberLength = Math.max(parseFloat(next).toString().length, parseInt(next).toString().length);
+      output += makeTags("number") + next.slice(0, numberLength);
+      mayBeDefault = true;
+      i += numberLength;
+    }else if(!commentOrString && (code[i] === "\n" || code[i] === "\r")){
+      output += "<br>";
+      i += 1;
+    }else{
+      if(mayBeDefault && code[i] !== " " && code[i] !== "\t"){
+        output += makeTags("default");
+        mayBeDefault = false;
+      }
+      output += code[i].replace("<", "&lt;").replace(">", "&gt;").replace("\n", "\\&#8203;n").replace("\r", "\\&#8203;r");
+      i += 1;
+    }
+  }
+
+  return (output + "</span>").slice(7);
+}
